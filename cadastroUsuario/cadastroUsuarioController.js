6575ddbddca860284e7347426a25985711a92edc
Fatima <fatima.carneiro@bluesoft.com.br>
2019-02-20 23:02:30 -0300

    apaguei os arquivos desnecessários e tentei estruturar um pouco para angular, mas não deu muito certo

--- cadastroUsuario/cadastroUsuarioController.js
+++
@@ -1,8 +0,0 @@
-//O QUE PRECISA SER FEITO AQUI:
-//1. Após o clique do botão 'salvar usuário', 
-//   salvar os dados inputados no service 
-//   work e no indexedDB.
-//2.Conferir se o cadastro já existe;
-//  2.1 se setTimeout, solta aviso ao 
-//      usuário e não cadastra novo usuário;
-//  2.2 senão, salva novo usuário e direciona para listaReceitaDespesa.
